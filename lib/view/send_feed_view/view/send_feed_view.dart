6255d97a38c8e8e9c2c04fb94c9cbc7a7150b74b
Abdullah Oğuz <oguzabdullah39@gmail.com>
2021-06-14 17:55:18 +0300

    send feed features updated

--- lib/view/send_feed_view/view/send_feed_view.dart
+++ lib/view/send_feed_view/view/send_feed_view.dart
@@ -1,13 +1,14 @@
 import 'dart:io';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:fluttermvvmtemplate/core/base/view/base_widget.dart';
 import 'package:fluttermvvmtemplate/core/components/text/auto_locale_text.dart';
 import 'package:fluttermvvmtemplate/core/extension/context_extension.dart';
 import 'package:fluttermvvmtemplate/core/init/lang/locale_keys.g.dart';
 import 'package:fluttermvvmtemplate/view/send_feed_view/viewmodel/send_feed_viewmodel.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:images_picker/images_picker.dart';
 import '../../../core/extension/string_extension.dart';
 
@@ -16,7 +17,10 @@ class SendFeedView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var res;
+    final _picker = ImagePicker();
+    dynamic _pickImageError;
+    ScrollController? controller;
+
     return BaseView<SendFeedViewModel>(
       viewModel: SendFeedViewModel(),
       onModelReady: (model) {
@@ -70,115 +74,167 @@ class SendFeedView extends StatelessWidget {
         ),
         body: Container(
           color: context.colors.background,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Expanded(
-                flex: 14,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Expanded(
-                      flex: 2,
-                      child: CircleAvatar(
-                        radius: 25,
-                        child: ClipOval(
-                          child: Image.network(
-                            'https://avatars.githubusercontent.com/u/34376691?v=4',
+          child: GestureDetector(
+            onTap: () {
+              print('ednemee');
+            },
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Expanded(
+                  flex: viewModel.image == null ? 14 : 300,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child: CircleAvatar(
+                          radius: 25,
+                          child: ClipOval(
+                            child: Image.network(
+                              'https://avatars.githubusercontent.com/u/34376691?v=4',
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    Expanded(
+                      Expanded(
                         flex: 10,
-                        child: TextFormField(
-                          autofocus: true,
-                          focusNode: viewModel.focus,
-                          maxLines: 20,
-                          decoration: InputDecoration(
-                              focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide.none),
-                              enabledBorder: OutlineInputBorder(
-                                  borderSide: BorderSide.none),
-                              labelStyle:
-                                  TextStyle(fontWeight: FontWeight.w300),
-                              hintStyle: TextStyle(
-                                  color: Colors.black26,
-                                  fontWeight: FontWeight.w500),
-                              hintText:
-                                  LocaleKeys.sendFeed_shareYourThought.locale),
-                        ))
-                  ],
-                ),
-              ),
-              res ??
-                  Container(
-                    color: Colors.red,
-                  ),
-              Container(
-                alignment: Alignment.centerRight,
-                child: Container(
-                  padding: context.paddingLowHorizontal,
-                  child: ElevatedButton(
-                      style: ButtonStyle(
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18.0),
+                        child: SingleChildScrollView(
+                          controller: controller,
+                          keyboardDismissBehavior:
+                              ScrollViewKeyboardDismissBehavior.onDrag,
+                          child: Column(
+                            children: [
+                              TextFormField(
+                                autofocus: true,
+                                decoration: InputDecoration(
+                                    focusedBorder: OutlineInputBorder(
+                                        borderSide: BorderSide.none),
+                                    enabledBorder: OutlineInputBorder(
+                                        borderSide: BorderSide.none),
+                                    labelStyle:
+                                        TextStyle(fontWeight: FontWeight.w300),
+                                    hintStyle: TextStyle(
+                                        color: Colors.black26,
+                                        fontWeight: FontWeight.w500),
+                                    hintText: LocaleKeys
+                                        .sendFeed_shareYourThought.locale),
+                              ),
+                              viewModel.image != null
+                                  ? Observer(builder: (_) {
+                                      return Container(
+                                        decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(20)),
+                                        width:
+                                            context.mediaQuery.size.width * 0.8,
+                                        child: SingleChildScrollView(
+                                          keyboardDismissBehavior:
+                                              ScrollViewKeyboardDismissBehavior
+                                                  .onDrag,
+                                          child: Image.file(
+                                            File(
+                                              viewModel.image!.path,
+                                            ),
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                      );
+                                    })
+                                  : Container(),
+                            ],
                           ),
                         ),
-                        padding: MaterialStateProperty.all(
-                          EdgeInsets.symmetric(
-                              horizontal: 0,
-                              vertical: context.mediaQuery.size.height * 0.020),
-                        ),
-                        backgroundColor:
-                            MaterialStateProperty.all(context.colors.primary),
-                      ),
-                      child: Icon(Icons.camera_enhance,
-                          color: context.colors.background),
-                      onPressed: () {
-                        showModalBottomSheet(
-                            context: context,
-                            builder: (BuildContext bc) {
-                              return SafeArea(
-                                child: Container(
-                                  // ignore: unnecessary_new
-                                  child: new Wrap(
-                                    children: <Widget>[
-                                      ListTile(
-                                          leading: Icon(Icons.photo_library),
-                                          title: Text('Photo Library'),
-                                          onTap: () async {
-                                            res = await ImagesPicker.pick(
-                                                pickType: PickType.all,
-                                                gif: true,
-                                                quality: 0.8,
-                                                maxSize: 500
-                                                // record video max time
-                                                );
-                                          }),
-                                      ListTile(
-                                        leading: Icon(Icons.photo_camera),
-                                        title: Text('Camera'),
-                                        onTap: () {
-                                          //  :
-                                          Navigator.of(context).pop();
-                                        },
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              );
-                            });
-                      }),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              Spacer(
-                flex: 13,
-              )
-            ],
+                Observer(builder: (_) {
+                  return Container(
+                    alignment: Alignment.centerRight,
+                    child: Container(
+                      padding: context.paddingLowHorizontal,
+                      child: ElevatedButton(
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18.0),
+                              ),
+                            ),
+                            padding: MaterialStateProperty.all(
+                              EdgeInsets.symmetric(
+                                  horizontal: 0,
+                                  vertical:
+                                      context.mediaQuery.size.height * 0.020),
+                            ),
+                            backgroundColor: MaterialStateProperty.all(
+                                context.colors.primary),
+                          ),
+                          child: Icon(Icons.camera_enhance,
+                              color: context.colors.background),
+                          onPressed: () {
+                            showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(32.0))),
+                                    actionsPadding: context.paddingNormal,
+                                    buttonPadding: context.paddingMedium,
+                                    actions: [
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceAround,
+                                        children: [
+                                          IconButton(
+                                              onPressed: () async {
+                                                try {
+                                                  final pickedFile =
+                                                      await await _picker
+                                                          .getImage(
+                                                    source: ImageSource.gallery,
+                                                  );
+                                                  viewModel
+                                                      .changeImage(pickedFile);
+                                                  Navigator.pop(context);
+                                                } catch (e) {
+                                                  print(e);
+                                                }
+                                              },
+                                              icon: Icon(Icons.photo_album)),
+                                          IconButton(
+                                              onPressed: () async {
+                                                try {
+                                                  final pickedFile =
+                                                      await await _picker
+                                                          .getImage(
+                                                              source:
+                                                                  ImageSource
+                                                                      .camera,
+                                                              imageQuality: 80);
+                                                  viewModel
+                                                      .changeImage(pickedFile);
+                                                  Navigator.pop(context);
+                                                } catch (e) {
+                                                  print(e);
+                                                }
+                                              },
+                                              icon: Icon(Icons.camera_alt))
+                                        ],
+                                      )
+                                    ],
+                                  );
+                                });
+                          }),
+                    ),
+                  );
+                }),
+                viewModel.image == null ? Spacer(flex: 13) : Container()
+              ],
+            ),
           ),
         ),
       ),
